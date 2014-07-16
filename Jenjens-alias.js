fa58cef27c6a0a526f0b55f55e937099a0bb38a0
Paul Liverman <paul.liverman.iii@gmail.com>
2014-07-16 16:16:16 -0700

    Create Jenjens-alias.js

---
+++ Jenjens-alias.js
@@ -0,0 +1,49 @@
+/*
+	Copyright 2013-2014 Paul Liverman III
+
+	This file is part of Jenjens.
+
+	Jenjens is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Jenjens is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Jenjens.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//color related
+color=Jenjens.color;
+Jenjens.color.random=Jenjens.random.color;
+//Maths!
+Math.Tau=Jenjens.Math.Tau;
+Math.getDistance=Jenjens.Math.getDistance;
+Math.toDegrees=Jenjens.Math.toDegrees;
+Math.toRadians=Jenjens.Math.toRadians;
+Vector=Jenjens.Math.Vector;
+//physics
+physics=Jenjens.physics;
+vector=Jenjens.physics.vector;
+//string related
+string=Jenjens.string;
+//randomization
+random=Jenjens.random;
+//rendering
+render=Jenjens.render;
+//structures
+Circle=Jenjens.structure.Circle;
+//io
+io=Jenjens.io;
+//utility functions
+forEach=Jenjens.util.forEach;
+forEach2=Jenjens.util.forEach2;
+forEachCompare=Jenjens.util.forEachCompare;
+Interval=Jenjens.util.Interval;
+Timer=Jenjens.util.Timer;
+//debug
+debug=Jenjens.debug;

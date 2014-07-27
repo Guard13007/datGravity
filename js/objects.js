8a6d237fefdbc078c5550ed1d5ab2f620500e575
Paul Liverman <paul.liverman.iii@gmail.com>
2014-07-27 16:02:20 -0700

    made link color better / working on ideas

--- js/objects.js
+++ js/objects.js
@@ -14,7 +14,7 @@ var Body=function(color,name,x,y,rotationSpeed){
 	x? this.x=x : this.x=0;
 	y? this.y=y : this.y=0;
 	rotationSpeed? this.rotationSpeed=rotationSpeed : this.rotationSpeed=0;
-	rotation=0;
+	this.rotation=0;
 };
 
 var Planetoid=function(radius,color,name,x,y,rotationSpeed){
@@ -31,6 +31,31 @@ var Ship=function(name,Parts,x,y,rotation){
 
 	Parts? this.Parts=Parts : this.Parts=[]; //should throw error? log error?
 	rotation? this.rotation=rotation : this.rotation=0;
+};
+
+/*
+	Body(color,name,x,y,rotationSpeed)
+	Planetoid(radius,color,name,x,y,rotationSpeed)
+	Ship(name,Vessel,x,y,rotation)
+	Part(width,height,color,name)
+	Body{
+		Planetoid - created with stats to be placed in orbit
+		Ship - created with stats to be placed on the surface of a base (current: create in an orbit)
+		Vessel - a ship without a position or physics, a design to be used later
+	}
+	Part{
+		created for use in later designs, shouldn't have placement,
+		 should be used to create the objects for later use
+		Tank - should be relatively low mass, a thin shell filled with a specific type of fuel or fuels
+		Thruster - should medium low mass, a thruster that fires to maneuver..this is probably stupid
+		 because I don't know how to do proper physics to adjust velocity/rotation based on placement
+		 of thruster
+		Engine - medium mass, fires based on throttle
+	}
+*/
+
+var Part=function(x,y,width,height,color,name,rotation){
+	//
 };
 
 var Tank=function(x,y,width,height,color,name,rotation){
@@ -54,6 +79,34 @@ var Tank=function(x,y,width,height,color,name,rotation){
 		4.512 * 0.9 = 4.0608
 		4.512 * 0.1 = 0.4512
 	*/
+};
+
+var Thruster=function(x,y,width,height,color,name,rotation){
+	this.type="Thruster";
+
+	color? this.color=color : this.color=[1,1,1,1];
+	name? this.name=name : this.name='unnamed thruster';
+	x? this.x=x : this.x=0;
+	y? this.y=y : this.y=0;
+
+	width? this.width=width : this.width=0.2;
+	height? this.height=height : this.height=0.1;
+	this.mass=0.3*this.width*this.height;
+	rotation? this.rotation=rotation : this.rotation=0;
+};
+
+var Engine=function(x,y,width,height,color,name,rotation){
+	this.type="Engine";
+
+	color? this.color=color : this.color=[1,1,1,1];
+	name? this.name=name : this.name='unnamed engine';
+	x? this.x=x : this.x=0;
+	y? this.y=y : this.y=0;
+
+	width? this.width=width : this.width=1;
+	height? this.height=height : this.height=0.4;
+	this.mass=0.5*this.width*this.height;
+	rotation? this.rotation=rotation : this.rotation=0;
 };
 
 console.log("Constructors (objects.js) loaded.");

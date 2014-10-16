70a476649b5e09b719803927d7f7bf26710162dc
Paul Liverman <paul.liverman.iii@gmail.com>
2014-10-15 21:34:22 -0700

    working on it

--- compile.bat
+++ compile.bat
@@ -1,7 +1,13 @@
 @ECHO OFF
+echo Removing old img folder...
+rmdir /S /Q "%cd%/img"
+
+echo Copying new img folder...
+xcopy /S /V /I /Q /Y "%cd%/source/img" "%cd%/img"
+
 echo Uglifying JS...
 IF EXIST node_modules/.bin/uglifyjs (
-	cmd /c ""%cd%/node_modules/.bin/uglifyjs" source/lib/dat.gui.min.js source/lib/Jenjens.js source/lib/Jenjens-alias.js source/gui.js source/images.js source/io.js source/main.js source/objects.js source/physics.js source/render.js source/Vessels.js --screw-ie8 --unsafe --mangle --output JS.js -v"
+	cmd /c ""%cd%/node_modules/.bin/uglifyjs" source/js/lib/dat.gui.min.js source/js/lib/Jenjens.js source/js/lib/Jenjens-alias.js source/js/gui.js source/js/images.js source/js/io.js source/js/main.js source/js/objects.js source/js/physics.js source/js/render.js source/js/Vessels.js --screw-ie8 --compress,sequences,properties,conditionals,comparisons,evaluate,booleans,loops,if_return,join_vars,cascade,warnings,negate_iife,pure_getters --unsafe --mangle --output JS.js -v"
 ) ELSE (
-	cmd /c uglifyjs source/lib/dat.gui.min.js source/lib/Jenjens.js source/lib/Jenjens-alias.js source/gui.js source/images.js source/io.js source/main.js source/objects.js source/physics.js source/render.js source/Vessels.js --screw-ie8 --unsafe --mangle --output JS.js -v
+	cmd /c uglifyjs source/js/lib/dat.gui.min.js source/js/lib/Jenjens.js source/js/lib/Jenjens-alias.js source/js/gui.js source/js/images.js source/js/io.js source/js/main.js source/js/objects.js source/js/physics.js source/js/render.js source/js/Vessels.js --screw-ie8 --compress,sequences,properties,conditionals,comparisons,evaluate,booleans,loops,if_return,join_vars,cascade,warnings,negate_iife,pure_getters --unsafe --mangle --output JS.js -v
 )

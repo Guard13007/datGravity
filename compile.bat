f7dfc5c8f7c08150aa1ef2446ac400f599944ded
Paul Liverman <paul.liverman.iii@gmail.com>
2014-10-15 21:36:42 -0700

    finished updating for minification/building

--- compile.bat
+++ compile.bat
@@ -11,3 +11,12 @@ IF EXIST node_modules/.bin/uglifyjs (
 ) ELSE (
 	cmd /c uglifyjs source/js/lib/dat.gui.min.js source/js/lib/Jenjens.js source/js/lib/Jenjens-alias.js source/js/gui.js source/js/images.js source/js/io.js source/js/main.js source/js/objects.js source/js/physics.js source/js/render.js source/js/Vessels.js --screw-ie8 --compress,sequences,properties,conditionals,comparisons,evaluate,booleans,loops,if_return,join_vars,cascade,warnings,negate_iife,pure_getters --unsafe --mangle --output JS.js -v
 )
+
+echo Documenting source...
+IF EXIST node_modules/.bin/jsdoc (
+	cmd /c ""%cd%/node_modules/.bin/jsdoc" source -d docs -r"
+) ELSE (
+	cmd /c jsdoc source -d docs -r
+)
+
+echo Finished.
